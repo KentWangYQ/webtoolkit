1dec3a43896658448d10dd179e8c3bec2dd61bf7
KentWangYQ <sdwyq1238@163.com>
2017-05-12 18:36:19 +0800

    update base model

--- ORM/BaseModel.cs
+++ ORM/BaseModel.cs
@@ -8,5 +8,16 @@ namespace ORM
     public class BaseModel
     {
         public int ID { get; set; }
+        public T ConvertTo<T>()
+        {
+            if (!this.GetType().IsSubclassOf(typeof(T)))
+                throw new Exception(string.Format("Can not Convert type {0} to type {1}", this.GetType().FullName, typeof(T).FullName));
+            var instance = Activator.CreateInstance<T>();
+            foreach (System.Reflection.PropertyInfo property in typeof(T).GetProperties())
+            {
+                property.SetValue(instance, this.GetType().GetProperty(property.Name).GetValue(this, null), null);
+            }
+            return instance;
+        }
     }
 }
